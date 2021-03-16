60bd654619550b782f6a994436d965dd82c72e1e
zezking <zhaoenze001@gmail.com>
2021-03-16 13:29:44 -0700

    finish interviewersList

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,10 +1,24 @@
 import React from "react";
 import "components/InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
 
-export default function InterviewerList() {
+export default function InterviewerList(props) {
+  const interviewer = props.interviewers
+    ? props.interviewers.map((interviewer, index) => {
+        return (
+          <InterviewerListItem
+            key={index}
+            name={interviewer.name}
+            avatar={interviewer.avatar}
+            setInterviewer={props.setInterviewer}
+          />
+        );
+      })
+    : "no interview available";
+
   return (
     <section className="interviewers">
-      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <h4 className="interviewers__header text--light">{interviewer}</h4>
       <ul className="interviewers__list"></ul>
     </section>
   );
--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import "index.scss";
 
 import Application from "components/Application";
 import DayListItem from "components/DayListItem";
+import InterviewerListItem from "components/InterviewerListItem";
 
 ReactDOM.render(
   <div>
     <Application />
     <DayListItem />
+    <InterviewerListItem />
   </div>,
   document.getElementById("root")
 );

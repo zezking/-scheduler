f7ad620bfba9c0ebf34e85c67a501a65d5902f7c
zezking <zhaoenze001@gmail.com>
2021-03-16 19:07:50 -0700

    finish form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,13 +6,22 @@ export default function Form(props) {
   const [interviewer, setInterviewer] = useState(props.interviewer || "");
   const [name, setName] = useState(props.name || "");
 
+  const reset = () => {
+    setName("");
+    setInterviewer(null);
+  };
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
         <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
-            name={name}
             type="text"
             placeholder="Enter Student Name"
             value={name}
@@ -27,10 +36,15 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>
+          <Button danger onClick={cancel}>
             Cancel
           </Button>
-          <Button confirm onClick={props.onSave}>
+          <Button
+            confirm
+            onClick={() => {
+              props.onSave(name, interviewer);
+            }}
+          >
             Save
           </Button>
         </section>

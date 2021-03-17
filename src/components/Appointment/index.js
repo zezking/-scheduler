3be69c28fe588859eb4a5b5ac8b648edbbf91828
zezking <zhaoenze001@gmail.com>
2021-03-16 20:14:54 -0700

    finished Show

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,22 @@
 import React from "react";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 import "components/Appointment/styles.scss";
+import { faCoins } from "@fortawesome/free-solid-svg-icons";
 
-export default function Appointment() {
-  return <article className="appointment"></article>;
+export default function Appointment(props) {
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer.name}
+        />
+      ) : (
+        <Empty />
+      )}
+    </article>
+  );
 }
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -18,6 +18,7 @@ import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
 import Form from "components/Appointment/Form";
+
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
@@ -141,11 +142,27 @@ storiesOf("Appointment", module)
     backgrounds: [{ name: "white", value: "#fff", default: true }],
   })
   .add("Appointment", () => <Appointment />)
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="12pm" />
+      <Appointment id="last" time="1pm" />
+    </Fragment>
+  ))
+  .add("Appointment Booked", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="12pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment id="last" time="1pm" />
+    </Fragment>
+  ))
   .add("Header...", () => <Header time="12PM" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")} />)
   .add("Show", () => (
     <Show
-      interviewers={interviewer}
+      interviewer={interviewer}
       name={interviewer.name}
       student={"Lydia Miller-Jones"}
       onEdit={action("onEidt")}

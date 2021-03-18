89c1dfc295e99201ae07d6c6967e607b4bf08c56
zezking <zhaoenze001@gmail.com>
2021-03-18 15:51:46 -0700

    finished interviewers

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,11 @@ import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "./Appointment/index";
 import axios from "axios";
-import { getAppointmentsForDay, getInterview } from "../helpers/selectors";
+import {
+  getAppointmentsForDay,
+  getInterview,
+  getInterviewersForDay,
+} from "../helpers/selectors";
 
 const URLs = {
   GET_DAYS: "http://localhost:8001/api/days",
@@ -86,6 +90,7 @@ export default function Application() {
   }, []);
 
   const appointments = getAppointmentsForDay(state, state.day);
+  const interviewers = getInterviewersForDay(state, state.day);
 
   const schedule = appointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
@@ -96,7 +101,7 @@ export default function Application() {
         id={appointment.id}
         time={appointment.time}
         interview={interview}
-        // interviewers={state.interviewers}
+        interviewers={interviewers}
       />
     );
   });

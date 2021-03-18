4f944bce302182fa0e8a5bf7615c97bd78ca1034
zezking <zhaoenze001@gmail.com>
2021-03-17 18:05:24 -0700

    refactor helpfer function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,23 +1,37 @@
 export function getAppointmentsForDay(state, day) {
-  const daysbyAppintment = [...state.days];
-  const appointmentList = { ...state.appointments };
+  //   const appointmentList = { ...state.appointments };
+
+  //this is the refactored code
   let appointmentForDay = [];
-  let dayAndAppointments = {};
-  for (const dayID of daysbyAppintment) {
-    if (!dayAndAppointments[dayID.name]) {
-      dayAndAppointments[dayID.name] = dayID.appointments;
-    }
+  const filteredDay = state.days.filter((index) => index.name === day);
+  if (!filteredDay) {
+    return appointmentForDay;
   }
-  for (const dayOfWeek in dayAndAppointments) {
-    if (dayOfWeek === day) {
-      for (const appointmentID of dayAndAppointments[day]) {
-        appointmentForDay.push(appointmentList[appointmentID]);
-      }
-    }
+  if (filteredDay.length > 0) {
+    filteredDay[0].appointments.map((id) => {
+      return appointmentForDay.push(state.appointments[id]);
+    });
   }
 
-  //needs refactored!
   return appointmentForDay;
+
+  //This is my ugly ass original code
+  //   let appointmentForDay = [];
+  //   let dayAndAppointments = {};
+  //   for (const dayID of daysbyAppintment) {
+  //     if (!dayAndAppointments[dayID.name]) {
+  //       dayAndAppointments[dayID.name] = dayID.appointments;
+  //     }
+  //   }
+  //   for (const dayOfWeek in dayAndAppointments) {
+  //     if (dayOfWeek === day) {
+  //       for (const appointmentID of dayAndAppointments[day]) {
+  //         appointmentForDay.push(appointmentList[appointmentID]);
+  //       }
+  //     }
+  //   }
+
+  //needs refactored!
 }
 export function getInterview(state, interview) {
   const interviewersList = { ...state.interviewers };

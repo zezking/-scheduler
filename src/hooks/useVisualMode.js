b6985ea4fa289f0bb04c6ad742ecfd7e707c3483
zezking <zhaoenze001@gmail.com>
2021-03-18 12:16:03 -0700

    finish the transition replace

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -11,15 +11,24 @@ export function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  const transition = (newMode) => {
+  const transition = (newMode, replace = false) => {
+    if (replace) {
+      history.pop();
+      setMode(initial);
+    }
     setMode(newMode);
-    let newHistory = [...history];
-    newHistory = [...history, newMode];
+    let newHistory = [...history, newMode];
+
     setHistory(newHistory);
   };
+
   const back = () => {
+    if (history.length < 2) {
+      return;
+    }
     history.pop();
     setMode(history[history.length - 1]);
   };
+
   return { mode, transition, back };
 }

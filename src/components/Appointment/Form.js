a004eaa4fe9a90f1d5f5cda6661d00f6cf52fd64
zezking <zhaoenze001@gmail.com>
2021-03-22 14:39:43 -0700

    add Form.test.js

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -16,16 +16,26 @@ export default function Form(props) {
     props.onCancel();
   };
 
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+
+    props.onSave(name, interviewer);
+  }
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
         <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
+            name="name"
             type="text"
             placeholder="Enter Student Name"
             value={name}
             onChange={(event) => setName(event.target.value)}
+            data-testid="student-name-input"
           />
         </form>
         <InterviewerList

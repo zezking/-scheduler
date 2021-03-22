1dd0d02b900de7042372bb69d0015a362b99013c
zezking <zhaoenze001@gmail.com>
2021-03-22 16:10:34 -0700

    finish test

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -37,33 +37,52 @@ describe("Form", () => {
     expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
     expect(onSave).not.toHaveBeenCalled();
   });
-
-  it("calls onSave function when the name is defined", () => {
+  it("can successfully save after trying to submit an empty student name", () => {
     const onSave = jest.fn();
-
-    const { getByText, queryByText } = render(
-      <Form interviewers={interviewers} onSave={onSave} name="Jason" />
+    const { getByText, getByPlaceholderText, queryByText } = render(
+      <Form interviewers={interviewers} onSave={onSave} />
     );
+
+    fireEvent.click(getByText("Save"));
+
+    expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
+    expect(onSave).not.toHaveBeenCalled();
+
+    fireEvent.change(getByPlaceholderText("Enter Student Name"), {
+      target: { value: "Lydia Miller-Jones" },
+    });
 
     fireEvent.click(getByText("Save"));
 
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
+
     expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith("Jason", null);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
   });
 
-  it("submits the name entered by the user", () => {
-    const onSave = jest.fn();
-    const { getByText, getByPlaceholderText } = render(
-      <Form interviewers={interviewers} onSave={onSave} />
+  it("calls onCancel and resets the input field", () => {
+    const onCancel = jest.fn();
+    const { getByText, getByPlaceholderText, queryByText } = render(
+      <Form
+        interviewers={interviewers}
+        name="Lydia Mill-Jones"
+        onSave={jest.fn()}
+        onCancel={onCancel}
+      />
     );
 
-    const input = getByPlaceholderText("Enter Student Name");
-
-    fireEvent.change(input, { target: { value: "Jason" } });
     fireEvent.click(getByText("Save"));
 
-    expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith("Jason", null);
+    fireEvent.change(getByPlaceholderText("Enter Student Name"), {
+      target: { value: "Lydia Miller-Jones" },
+    });
+
+    fireEvent.click(getByText("Cancel"));
+
+    expect(queryByText(/student name cannot be blank/i)).toBeNull();
+
+    expect(getByPlaceholderText("Enter Student Name")).toHaveValue("");
+
+    expect(onCancel).toHaveBeenCalledTimes(1);
   });
 });

9e8daa2469a4a18ee039b3ea250802cfaecea765
zezking <zhaoenze001@gmail.com>
2021-03-24 17:38:48 -0700

    setup netlify

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 
 import "index.scss";
 
 import Application from "components/Application";
 import DayListItem from "components/DayListItem";
 import InterviewerListItem from "components/InterviewerListItem";
-
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
 ReactDOM.render(
   <div>
     <Application />
